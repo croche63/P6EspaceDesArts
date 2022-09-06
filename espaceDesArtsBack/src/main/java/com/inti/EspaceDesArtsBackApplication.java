62271cbf6675da1c2b3dcd459a0a9f3a6179fb3d
croche63 <cleroche63@gmail.com>
2022-09-06 10:03:35 +0200

    Creation projet

---
+++ espaceDesArtsBack/src/main/java/com/inti/EspaceDesArtsBackApplication.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspaceDesArtsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspaceDesArtsBackApplication.class, args);
+	}
+
+}
